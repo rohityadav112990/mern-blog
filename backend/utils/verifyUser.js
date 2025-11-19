123e2955fe2e56f2ac0100942091cd191d1a2980
rohityadav112990 <157958354+rohityadav112990@users.noreply.github.com>
2025-11-19 20:29:46 +0530

    Create verifyUser.js

---
+++ backend/utils/verifyUser.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  // console.log(token)
+  if (!token) {
+    return next(errorHandler(401, "Unauthorized (Token Not Found)"));
+  }
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) {
+      return next(errorHandler(401, "Unauthorized (Token Not Valid)"));
+
+    }
+      req.user = user ;
+      next()
+  });
+};

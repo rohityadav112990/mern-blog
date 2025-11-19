51269bb0871e040d95f4fa6034a5bef735dfbc0c
rohityadav112990 <157958354+rohityadav112990@users.noreply.github.com>
2025-11-19 20:34:38 +0530

    Create user.models.js

---
+++ backend/models/user.models.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const defaultImagePath = 'https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; 
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: defaultImagePath,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
